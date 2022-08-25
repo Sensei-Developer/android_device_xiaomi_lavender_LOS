2b957673296ecb040e009b3ad865bd1be4f965af
1582130940 <19632674+1582130940@users.noreply.github.com>
2022-08-25 22:46:48 +0800

    lavender: gps: Update GPS from LA.UM.9.2.r1-03700-SDMxx0.0
    * gps: Update GPS config from DDV_sprout 00WW_3_690
    * gps: Resolve unused parameter warnings
    * gps: Ignore some build warnings
    * gps: Fix Wbitwise-instead-of-logical introduced by clang-r445002
    * gps: Add cflags "-Wno-error=enum-compare"
    
    Co-authored-by: Vivekachooz <vivekachooz@gmail.com>
    Co-authored-by: Pirama Arumuga Nainar <pirama@google.com>
    Co-authored-by: me-cafebabe <me_cafebabe@hotmail.com>

--- device.mk
+++ device.mk
@@ -224,23 +224,25 @@ PRODUCT_PACKAGES += \
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
 # GPS / Location
+$(call inherit-product, $(LOCAL_PATH)/gps/gps_vendor_product.mk)
+
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
-    libbatching \
-    libgeofencing \
-    libgnss \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    libgnsspps \
+    libsynergy_loc_api \
     libsensorndkbridge \
     libwifi-hal-ctrl
 
 # GPS Config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+PRODUCT_PACKAGES += \
+    apdr.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
 
 # Health
 PRODUCT_PACKAGES += \
